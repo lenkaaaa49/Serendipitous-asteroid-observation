36230c53a4bac3141c5a30c8550dc8da8c8ff387
Lenka Husarova <lenka@mac-055.sron.rug.nl>
2018-01-17 16:31:16 +0100

    Added 4th function to update old data(from xy date) and add new

--- Choosing_theFunction.py
+++ Choosing_theFunction.py
@@ -5,55 +5,80 @@ Created on Thu Dec  7 15:18:20 2017
 
 @author: lenka
 """
+import pymysql
 
 import Open_InputDatabase
 import importingdata_intoMYSQL
 
 
-def choose_function(password,Option,Older_than_date):
+def choose_function(password,Option,Older_than_date=None):
     #password='34GH2B.'
     #Option=2
     #Older_than_date=DateTime('2018/01/17 15:50:00')
-    #import input data from MIGO table
-    Special_id1,Obs_date,Vertex1,Vertex2,Vertex3,Vertex4,Status,Updated_Date=Open_InputDatabase.getinputs(password)
-    count=0
+    Special_id1=[]
+    try:
+        # Open database connection
+        db = pymysql.connect("localhost","root",password)
+        # disconnect from server
+        db.close() 
     
-    #add new lines (new data)
-    if Option==1:
-        #loop over each special ID
-        for ii in range (0,len(Special_id1)):
-            if Status[ii]!='UPDATED':
-                Special_ID=importingdata_intoMYSQL.importing_data(Option,password,Obs_date[ii],Special_id1[ii],Vertex1[ii],Vertex2[ii],Vertex3[ii],Vertex4[ii])
-    
-        if 'NULL' not in Status:
-            print('No new data to add')
-            
-    #go over data older than xyDate and update them/add them
-    elif Option==2:
-        #loop over each special ID
-        for ii in range (0,len(Special_id1)):
-            if Status[ii]=='UPDATED':
-                if Updated_Date[ii]<Older_than_date:
+        #import input data from MIGO table
+        Special_id1,Obs_date,Vertex1,Vertex2,Vertex3,Vertex4,Status,Updated_Date=Open_InputDatabase.getinputs(password)
+        count=0
+        
+        #add new lines (new data)
+        if Option==1:
+            #loop over each special ID
+            for ii in range (0,len(Special_id1)):
+                if Status[ii]!='UPDATED':
                     Special_ID=importingdata_intoMYSQL.importing_data(Option,password,Obs_date[ii],Special_id1[ii],Vertex1[ii],Vertex2[ii],Vertex3[ii],Vertex4[ii])
-                else:
-                    count=count+1
-                    if count==len(Special_id1):
-                        print('No outdated tables')
-                 
-    #adds new data and updates all the outdated ones    
-    elif Option==3:
-        #loop over each special ID
-        for ii in range (0,len(Special_id1)):
-            Special_ID=importingdata_intoMYSQL.importing_data(Option,password,Obs_date[ii],Special_id1[ii],Vertex1[ii],Vertex2[ii],Vertex3[ii],Vertex4[ii])
-    
-    #adds new data and go over data older than xyDate and update them/add them
-    elif Option==4:
-        #loop over each special ID
-        for ii in range (0,len(Special_id1)):
-            if Updated_Date[ii]<Older_than_date or Status[ii]=='NULL':
+        
+            if 'NULL' not in Status:
+                print('No new data to add')
+                
+        #go over data older than xyDate and update them/add them
+        elif Option==2:
+            if Older_than_date==None:
+                print ('Check your inputs (date)') 
+            else:
+                #loop over each special ID
+                for ii in range (0,len(Special_id1)):
+                    if Status[ii]=='UPDATED':
+                        if Updated_Date[ii]<Older_than_date:
+                            Special_ID=importingdata_intoMYSQL.importing_data(Option,password,Obs_date[ii],Special_id1[ii],Vertex1[ii],Vertex2[ii],Vertex3[ii],Vertex4[ii])
+                        else:
+                            count=count+1
+                            if count==len(Special_id1):
+                                print('No outdated tables')
+                     
+        #adds new data and updates all the outdated ones    
+        elif Option==3:
+            #loop over each special ID
+            for ii in range (0,len(Special_id1)):
                 Special_ID=importingdata_intoMYSQL.importing_data(Option,password,Obs_date[ii],Special_id1[ii],Vertex1[ii],Vertex2[ii],Vertex3[ii],Vertex4[ii])
-                   
-    else:
-        print("Check your inputs")
+            if 'NULL' not in Status:
+                print('No new data to add')
+                
+        #adds new data and go over data older than xyDate and update them/add them
+        elif Option==4:
+            if Older_than_date==None:
+                print ('Check your inputs (date)')
+            else:
+                #loop over each special ID
+                for ii in range (0,len(Special_id1)):
+                    if Updated_Date[ii]<Older_than_date or Status[ii]=='NULL':
+                        Special_ID=importingdata_intoMYSQL.importing_data(Option,password,Obs_date[ii],Special_id1[ii],Vertex1[ii],Vertex2[ii],Vertex3[ii],Vertex4[ii])
+                    else:
+                        count=count+1
+                        if count==len(Special_id1):
+                            print('No outdated tables and no new data to add')
+                if 'NULL' not in Status:
+                    print('No new data to add')      
+                    
+        else:
+            print("Check your inputs (Option)")
+        
+    except:
+        print ('Check your inputs (password)')
       
     return Special_id1
--- Run_thescript_tomake_database.py
+++ Run_thescript_tomake_database.py
@@ -13,4 +13,8 @@ zones = set(Timezones())
 #get all the names of the new made tables and run all the functions to 
 #create the database and tables
 Special_id=Choosing_theFunction.choose_function('34GH2B.',3,DateTime('2018/01/17 15:50:00'))
+#need:  password to MYSQL database
+#       function number (1-4)
+#       date and time in DateTime to determine which tables are outdated (if data older than this, they are updated)
+#               can be empty for function 1 and 3
 print ('Table names:',Special_id)
