a16ebaa4cd2bf0722d8869c3e56ca7de1c882816
Lenka Husarova <lenka@mac-055.sron.rug.nl>
2018-02-22 11:57:26 +0100

    User manual updated

--- Run_Script_Tomake_Database.py
+++ Run_Script_Tomake_Database.py
@@ -26,13 +26,13 @@ lambdaMu=[3.6,15,20]#wavelenghts until 29.9999 and only 4 decimal points
 #print ('Table names:',Special_id)
 
 #run the functions
-#Special_id=Different_Functions.create_new_tables('Login_Data.txt','Input_Data.txt',lambdaMu)
+Special_id=Different_Functions.create_new_tables('Login_Data.txt','Input_Data.txt',lambdaMu)
 #add new lines (new data) in the database
 
 #Special_id=Different_Functions.update_old_tables('Login_Data.txt','Input_Data.txt',DateTime('2018/02/02 11:36:00 ZULU'),lambdaMu)
 #go over data older than xyDate and update them/add them
 
-Special_id=Different_Functions.add_and_update_tables('Login_Data.txt','Input_Data.txt',lambdaMu)
+#Special_id=Different_Functions.add_and_update_tables('Login_Data.txt','Input_Data.txt',lambdaMu)
 #adds new data and updates all the outdated ones 
 
 #Special_id=Different_Functions.add_and_update_tables_after_date('Login_Data.txt','Input_Data.txt',DateTime('2018/01/23 16:56:00'),lambdaMu)
